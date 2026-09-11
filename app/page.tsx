9b2782beaee0e233c5cb98be56a9c9b10d5c17f8
takahiro888 <takahiro.45666@gmail.com>
2026-09-12 01:36:20 +0900

    test: 動作確認

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-primary">Buttonテスト</button>
     </div>
   );
 }
