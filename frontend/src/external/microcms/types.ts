2ba16b7452b5ab48e19a50184e59fd0b804aa53b
takahiro888 <takahiro.45666@gmail.com>
2026-09-21 14:50:24 +0900

    fix: types更新

--- frontend/src/external/microcms/types.ts
+++ frontend/src/external/microcms/types.ts
@@ -1,7 +1,17 @@
+export const CATEGORIES = ["React", "TypeScript", "Next.js", "CSS", "Testing","Git"];
+
+export type Category = {
+  id: string;
+  name: string;
+};
+
 export type Blog = {
   id: string;
   title: string;
   content: string;
+  excerpt?: string;
+  categories?: Category[];
+  readingMinutes?: number;
   eyecatch?: {
     url: string;
     width: number;
